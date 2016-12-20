e1dd0e5e65d505648c060629f91b506bc0f0c405
qinzhihao <scottzhq@163.com>
2016-12-20 11:27:28 +0800

    validates title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+
   def index
     @groups = Group.all
   end
@@ -17,14 +18,20 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def update
     @group = Group.find(params[:id])
-    @group.update(group_params)
-    redirect_to groups_path, notice: "Updated Success"
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Updated Success"
+    else
+      render :edit
+    end 
   end
 
   def destroy
@@ -32,7 +39,7 @@ class GroupsController < ApplicationController
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
-  end 
+  end
 
 
       private

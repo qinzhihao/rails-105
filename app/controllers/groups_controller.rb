d55ad71cc366facbd9d700985d79b8c14b2b5502
qinzhihao <scottzhq@163.com>
2016-12-20 16:22:44 +0800

    implement devise user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,8 @@
 class GroupsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only:[:edit, :update, :destroy]
+
   def index
     @groups = Group.all
   end
@@ -9,7 +12,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def show
@@ -18,6 +20,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
     if @group.save
       redirect_to groups_path
     else
@@ -26,16 +29,14 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to groups_path, notice: "Updated Success"
     else
       render :edit
-    end 
+    end
   end
 
   def destroy
-    @group = Group.find(params[:id])
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
@@ -46,6 +47,14 @@ class GroupsController < ApplicationController
 
       def group_params
         params.require(:group).permit(:title, :description)
+      end
+
+      def find_group_and_check_permission
+        @group = Group.find(params[:id])
+
+        if current_user != @group.user
+          redirect_to root_path, alert: "You have no permission"
+        end
       end
 
 
